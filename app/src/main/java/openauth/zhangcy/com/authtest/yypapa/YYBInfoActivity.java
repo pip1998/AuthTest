efdd83a8db6c5e2f303243a702dd0a977a21f549
pip1998 <pip1998@foxmail.com>
2015-11-13 15:27:09 +0800

    代码清理

--- app/src/main/java/openauth/zhangcy/com/authtest/yypapa/YYBInfoActivity.java
+++ app/src/main/java/openauth/zhangcy/com/authtest/yypapa/YYBInfoActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import openauth.zhangcy.com.authtest.Contacts;
@@ -49,27 +47,5 @@ public class YYBInfoActivity extends Activity {
         super.onNewIntent(intent);
         setIntent(intent);
         textView.setText("auth result: " + intent.getData().toString());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_yybinfo, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
